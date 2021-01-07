3e98e4d40ceae059d1dea3e20730cead5115325c
Admeo Logmo <loadgoca@gmail.com>
2021-01-07 12:14:23 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
+import Card from './Card';
+import GuessCount from './GuessCount';
+import './Card.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  render() {
+    return( 
+    <div className="memory" >
+      <GuessCount guesses={0} />
+      <Card card="🙂" feedback="hidden" />
+      <Card card="🎭" feedback="justMatched" />
+      <Card card="🤕" feedback="justMismatched" />
+      <Card card="🐥" feedback="visible" />
+      <Card card="🍩" feedback="hidden" />
+      <Card card="F" feedback="justMatched" />
+
     </div>
-  );
+    )
+  }
 }
+
 
 export default App;
